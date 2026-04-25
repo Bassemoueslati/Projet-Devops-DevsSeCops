de33d1f7910ebcab74398b6d2beaa049211b2f16
TaborskiM/ <taborski46@gmail.com>
2026-04-25 17:18:20 +0100

    errors fixed

--- src/main/java/tn/esprit/rh/achat/controllers/CategorieProduitController.java
+++ src/main/java/tn/esprit/rh/achat/controllers/CategorieProduitController.java
@@ -1,7 +1,6 @@
 package tn.esprit.rh.achat.controllers;
 
 import io.swagger.annotations.Api;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import tn.esprit.rh.achat.entities.CategorieProduit;
 import tn.esprit.rh.achat.services.ICategorieProduitService;
@@ -13,47 +12,38 @@ import java.util.List;
 @RequestMapping("/categorieProduit")
 public class CategorieProduitController {
 
-	
+    private final ICategorieProduitService categorieProduitService;
 
-	private final ICategorieProduitService categorieProduitService;
-
-    // ✅ Constructor Injection (instead of @Autowired)
+    // Constructor Injection
     public CategorieProduitController(ICategorieProduitService categorieProduitService) {
         this.categorieProduitService = categorieProduitService;
     }
 
-    // GET ALL
     @GetMapping("/retrieve-all-categorieProduit")
     public List<CategorieProduit> getCategorieProduit() {
         return categorieProduitService.retrieveAllCategorieProduits();
     }
 
-    // GET BY ID
     @GetMapping("/retrieve-categorieProduit/{categorieProduit-id}")
     public CategorieProduit retrieveCategorieProduit(
             @PathVariable("categorieProduit-id") Long categorieProduitId) {
         return categorieProduitService.retrieveCategorieProduit(categorieProduitId);
     }
 
-    // ADD
     @PostMapping("/add-categorieProduit")
     public CategorieProduit addCategorieProduit(@RequestBody CategorieProduit cp) {
         return categorieProduitService.addCategorieProduit(cp);
     }
 
-    // DELETE
     @DeleteMapping("/remove-categorieProduit/{categorieProduit-id}")
     public void removeCategorieProduit(
             @PathVariable("categorieProduit-id") Long categorieProduitId) {
         categorieProduitService.deleteCategorieProduit(categorieProduitId);
     }
 
-    // UPDATE
     @PutMapping("/modify-categorieProduit")
     public CategorieProduit modifyCategorieProduit(
             @RequestBody CategorieProduit categorieProduit) {
         return categorieProduitService.updateCategorieProduit(categorieProduit);
     }
-
-	
 }
--- src/main/java/tn/esprit/rh/achat/controllers/FournisseurRestController.java
+++ src/main/java/tn/esprit/rh/achat/controllers/FournisseurRestController.java
@@ -1,63 +1,50 @@
 package tn.esprit.rh.achat.controllers;
 
 import io.swagger.annotations.Api;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import tn.esprit.rh.achat.entities.Fournisseur;
 import tn.esprit.rh.achat.services.IFournisseurService;
 
 import java.util.List;
 
-
 @RestController
 @Api(tags = "Gestion des fournisseurss")
 @RequestMapping("/fournisseur")
 public class FournisseurRestController {
 
-	@Autowired
-	IFournisseurService fournisseurService;
+    private final IFournisseurService fournisseurService;
 
-	// http://localhost:8089/SpringMVC/fournisseur/retrieve-all-fournisseurs
-	@GetMapping("/retrieve-all-fournisseurs")
-	@ResponseBody
-	public List<Fournisseur> getFournisseurs() {
-		List<Fournisseur> fournisseurs = fournisseurService.retrieveAllFournisseurs();
-		return fournisseurs;
-	}
+    public FournisseurRestController(IFournisseurService fournisseurService) {
+        this.fournisseurService = fournisseurService;
+    }
 
-	// http://localhost:8089/SpringMVC/fournisseur/retrieve-fournisseur/8
-	@GetMapping("/retrieve-fournisseur/{fournisseur-id}")
-	@ResponseBody
-	public Fournisseur retrieveFournisseur(@PathVariable("fournisseur-id") Long fournisseurId) {
-		return fournisseurService.retrieveFournisseur(fournisseurId);
-	}
+    @GetMapping("/retrieve-all-fournisseurs")
+    public List<Fournisseur> getFournisseurs() {
+        return fournisseurService.retrieveAllFournisseurs();
+    }
 
-	// http://localhost:8089/SpringMVC/fournisseur/add-fournisseur
-	@PostMapping("/add-fournisseur")
-	@ResponseBody
-	public Fournisseur addFournisseur(@RequestBody Fournisseur f) {
-		Fournisseur fournisseur = fournisseurService.addFournisseur(f);
-		return fournisseur;
-	}
+    @GetMapping("/retrieve-fournisseur/{fournisseur-id}")
+    public Fournisseur retrieveFournisseur(@PathVariable("fournisseur-id") Long fournisseurId) {
+        return fournisseurService.retrieveFournisseur(fournisseurId);
+    }
 
-	// http://localhost:8089/SpringMVC/fournisseur/remove-fournisseur/{fournisseur-id}
-	@DeleteMapping("/remove-fournisseur/{fournisseur-id}")
-	@ResponseBody
-	public void removeFournisseur(@PathVariable("fournisseur-id") Long fournisseurId) {
-		fournisseurService.deleteFournisseur(fournisseurId);
-	}
+    @PostMapping("/add-fournisseur")
+    public Fournisseur addFournisseur(@RequestBody Fournisseur f) {
+        return fournisseurService.addFournisseur(f);
+    }
 
-	// http://localhost:8089/SpringMVC/fournisseur/modify-fournisseur
-	@PutMapping("/modify-fournisseur")
-	@ResponseBody
-	public Fournisseur modifyFournisseur(@RequestBody Fournisseur fournisseur) {
-		return fournisseurService.updateFournisseur(fournisseur);
-	}
+    @DeleteMapping("/remove-fournisseur/{fournisseur-id}")
+    public void removeFournisseur(@PathVariable("fournisseur-id") Long fournisseurId) {
+        fournisseurService.deleteFournisseur(fournisseurId);
+    }
 
-	// http://localhost:8089/SpringMVC/fournisseur/assignSecteurActiviteToFournisseur/1/5
-		@PutMapping(value = "/assignSecteurActiviteToFournisseur/{idSecteurActivite}/{idFournisseur}")
-		public void assignProduitToStock(@PathVariable("idSecteurActivite") Long idSecteurActivite, @PathVariable("idFournisseur") Long idFournisseur) {
-			fournisseurService.assignSecteurActiviteToFournisseur(idSecteurActivite, idFournisseur);
-		}
+    @PutMapping("/modify-fournisseur")
+    public Fournisseur modifyFournisseur(@RequestBody Fournisseur fournisseur) {
+        return fournisseurService.updateFournisseur(fournisseur);
+    }
 
+    @PutMapping("/assignSecteurActiviteToFournisseur/{idSecteurActivite}/{idFournisseur}")
+    public void assignProduitToStock(@PathVariable Long idSecteurActivite, @PathVariable Long idFournisseur) {
+        fournisseurService.assignSecteurActiviteToFournisseur(idSecteurActivite, idFournisseur);
+    }
 }
